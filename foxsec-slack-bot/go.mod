5d32b7f9f087b418a4ef3e3c331f9e5b7f3db84a
AJ Bahnken <aj@ajvb.me>
2019-02-19 16:12:17 -0800

    fixes for cloudfunction deployment

---
+++ foxsec-slack-bot/go.mod
@@ -0,0 +1,15 @@
+module github.com/mozilla-services/foxsec-pipeline-contrib/foxsec-slack-bot
+
+require (
+	github.com/aws/aws-sdk-go v1.17.1
+	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/lusis/go-slackbot v0.0.0-20180109053408-401027ccfef5 // indirect
+	github.com/lusis/slack-test v0.0.0-20180109053238-3c758769bfa6 // indirect
+	github.com/nlopes/slack v0.5.0
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/sirupsen/logrus v1.3.0
+	go.mozilla.org/mozlog v0.0.0-20170222151521-4bb13139d403 // indirect
+	go.mozilla.org/mozlogrus v1.0.0
+	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd // indirect
+	golang.org/x/text v0.3.0 // indirect
+)
--- foxsec-slack-bot/slackbot.go
+++ foxsec-slack-bot/slackbot.go
@@ -1,9 +1,16 @@
-package main
+package foxsecslackbot
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"net"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/mozilla-services/foxsec-pipeline-contrib/common"
 
@@ -19,11 +26,15 @@ import (
 
 var (
 	globalConfig Config
-	KEYNAME      = os.Getenv("KMS_KEYNAME")
+	client       = &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	KEYNAME = os.Getenv("KMS_KEYNAME")
 )
 
 const (
-	EMAIL_CHAR_SET = "UTF-8"
+	EMAIL_CHAR_SET             = "UTF-8"
+	WHITELIST_IP_SLASH_COMMAND = "whitelist_ip"
 )
 
 func init() {
@@ -148,6 +159,108 @@ func emailEscalation(alert *common.Alert) error {
 		return err
 	}
 
+	return nil
+}
+
+func handleAuthConfirm(req slack.InteractionCallback, db *common.DBClient) (*slack.Msg, error) {
+	alertId := strings.Split(req.CallbackID, "_")[1]
+	alert, err := db.GetAlert(alertId)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	response := "Error responding; please contact SecOps (secops@mozilla.com)"
+	if req.Actions[0].Name == "auth_yes" {
+		err := db.UpdateAlert(alert, "ACKNOWLEDGED")
+		if err != nil {
+			log.Error(err)
+		}
+		response = "Thank you for responding! Alert acknowledged"
+	} else if req.Actions[0].Name == "auth_no" {
+		err := escalateAlert(alert, db)
+		if err != nil {
+			log.Error(err)
+		}
+		response = "Thank you for responding! Alert has been escalated to SecOps (secops@mozilla.com)"
+	}
+
+	return &slack.Msg{Text: response, ReplaceOriginal: false}, nil
+}
+
+func readInteraction(r *http.Request) (slack.InteractionCallback, error) {
+	// Handle interaction callback
+	buf, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Error(err.Error())
+		return slack.InteractionCallback{}, err
+	}
+	var req slack.InteractionCallback
+	err = json.Unmarshal(buf, &req)
+	if err != nil {
+		log.Error(err.Error())
+		return slack.InteractionCallback{}, err
+	}
+
+	return req, nil
+}
+
+func isAuthConfirm(req slack.InteractionCallback) bool {
+	if strings.HasPrefix(req.CallbackID, "auth_confirmation") {
+		return true
+	}
+
+	return false
+}
+
+func handleWhitelistCmd(cmd slack.SlashCommand, db *common.DBClient) (*slack.Msg, error) {
+	msg := &slack.Msg{}
+
+	splitCmd := strings.Split(cmd.Text, " ")
+	ip := net.ParseIP(splitCmd[0])
+	if ip == nil {
+		m := fmt.Sprintf("Got invalid IP: %s", splitCmd[0])
+		msg.Text = m
+		return msg, errors.New(m)
+	}
+
+	expiresDur, err := time.ParseDuration(splitCmd[1])
+	if err != nil {
+		log.Error(err.Error())
+		msg.Text = "Was unable to correctly parse duration"
+		return msg, err
+	}
+	expiresAt := time.Now().Add(expiresDur)
+
+	userProfile, err := globalConfig.slackClient.GetUserProfile(cmd.UserID, false)
+	if err != nil {
+		log.Error(err.Error())
+		msg.Text = "Was unable to get your email from Slack."
+		return msg, err
+	}
+
+	err = db.SaveWhitelistedIp(common.NewWhitelistedIP(ip.String(), expiresAt, userProfile.Email))
+	if err != nil {
+		log.Error(err.Error())
+		msg.Text = "Error saving IP to whitelist"
+		return msg, err
+	}
+
+	msg.Text = fmt.Sprintf("Successfully saved %s to the whitelist. Will expire at %s", ip, expiresAt)
+	return msg, nil
+}
+
+func sendSlackCallback(msg *slack.Msg, responseUrl string) error {
+	j, err := json.Marshal(msg)
+	if err != nil {
+		log.Error(err.Error())
+		return err
+	}
+	_, err = client.Post(responseUrl, "application/json", bytes.NewBuffer(j))
+	if err != nil {
+		log.Error(err.Error())
+		return err
+	}
 	return nil
 }
 
